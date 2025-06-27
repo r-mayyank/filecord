ac36739ae591dd88b2ef7cd1d288762d8615c7bf
r_mayyank <thelueblue@gmail.com>
2025-06-28 04:24:15 +0530

    build

--- components/ui/Toast/use-toast.ts
+++ components/ui/Toast/use-toast.ts
@@ -1,14 +1,14 @@
 import { toast as sonnerToast } from "sonner";
 
-const toast = (message, data) => {
+const toast = (message: string, data?: any) => {
   return sonnerToast(message, data);
 };
 
-toast.success = (message, data) => sonnerToast.success(message, data);
-toast.error = (message, data) => sonnerToast.error(message, data);
-toast.warning = (message, data) => sonnerToast.warning(message, data);
-toast.info = (message, data) => sonnerToast.info(message, data);
-toast.loading = (message, data) => sonnerToast.loading(message, data);
-toast.dismiss = (id) => sonnerToast.dismiss(id);
+toast.success = (message: string, data?: any) => sonnerToast.success(message, data);
+toast.error = (message: string, data?: any) => sonnerToast.error(message, data);
+toast.warning = (message: string, data?: any) => sonnerToast.warning(message, data);
+toast.info = (message: string, data?: any) => sonnerToast.info(message, data);
+toast.loading = (message: string, data?: any) => sonnerToast.loading(message, data);
+toast.dismiss = (id?: string | number) => sonnerToast.dismiss(id);
 
 export { toast };
